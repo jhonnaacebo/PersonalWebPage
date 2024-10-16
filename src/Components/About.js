1a7eb1707bdcc19957e36c2111e83acab9e68f6b
jhonnaacebo <acebojhonna72@gmail.com>
2024-10-16 09:50:43 +0800

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import './Styles.css';
 
 const About = () => (
-  <Container>
-    <h2 style={{ fontFamily: 'Trebuchet MS', fontWeight: 'bold' }}>
-      About Me
-    </h2>
-    <p style={{ fontFamily: 'Georgia, serif', fontStyle: 'italic' }}>
-      My name is Jhonna Acebo, a 20 year old third year Computer Science Stduent at University of Cabuyao. 
+  <Container className='about-container'>
+    <h2 className="h2">About Me</h2>
+    <p className="p">My name is Jhonna Acebo, a 20 year old third year Computer Science Stduent at University of Cabuyao. 
       I am currently learning Web Development.
     </p>
   </Container>
