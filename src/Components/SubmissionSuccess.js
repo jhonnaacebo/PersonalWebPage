b1ed4b5f4b9d21313e4fbf6cde386489538b942c
jhonnaacebo <acebojhonna72@gmail.com>
2024-10-16 09:52:20 +0800

    Update SubmissionSuccess.js

--- src/Components/SubmissionSuccess.js
+++ src/Components/SubmissionSuccess.js
@@ -7,13 +7,11 @@ const SubmissionSuccess = () => {
   const { name, email, message } = location.state || {};
 
   return (
-    <Container>
-       <h2 style={{ fontFamily: 'Trebuchet MS', fontWeight: 'bold', color: 'black' }}>
-      Thank you, {name}!
-    </h2>
-      <p style={{ fontFamily: 'Trebuchet MS' }}>- Your message has been sent successfully. I'll get back to you as soon as possible.</p>
-      <p style={{ fontFamily: 'Trebuchet MS' }}><strong>Email:</strong> {email}</p>
-      <p style={{ fontFamily: 'Trebuchet MS' }}><strong>Message:</strong> {message}</p>
+    <Container className="submission-success-container">
+       <h2 className="h2">Thank you, {name}!</h2>
+      <p className="p">- Your message has been sent successfully. I'll get back to you as soon as possible.</p>
+      <p className="p1"><strong>Email:</strong> {email}</p>
+      <p className="p1"><strong>Message:</strong> {message}</p>
     </Container>
   );
 };
