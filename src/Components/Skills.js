08db3c44172e5f3cc14f1bb31533522425537d37
jhonnaacebo <acebojhonna72@gmail.com>
2024-10-16 09:51:15 +0800

    Update Skills.js

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import './Styles.css';
 
 const Skills = () => (
-  <Container>
-    <h2 style={{ fontFamily: 'Trebuchet MS', fontWeight: 'bold' }}>My Skills</h2>
-    <p style={{ fontFamily: 'Georgia, serif', fontStyle: 'italic'}}>Here are the technologies and skills I have experience with:</p>
-    <ul style={{ fontFamily: 'Trebuchet MS'}}>
+  <Container className="skills-container">
+    <h2 className="h2">My Skills</h2>
+    <p className="p">Here are the technologies and skills I have experience with:</p>
+    <ul className="skills-list">
       <li>CSS</li>
       <li>React</li>
       <li>HTML</li>
-      <li>Basic JavaScript</li>
+      <li>JavaScript</li>
       <li>Graphic Design</li>
-      <li>Basic Java</li>
+      <li>Java</li>
+      <li>Blender</li>
+      <li>Git</li>
     </ul>
   </Container>
 );
 
 export default Skills;
-
