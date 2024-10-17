2d6254f541e1340446b855e5bca6b483b7b662aa
jhonnaacebo <acebojhonna72@gmail.com>
2024-10-17 09:17:24 +0800

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import React from 'react'; // Required for using JSX and React component features.
+import { Container } from 'react-bootstrap'; // Bootstrap's Container component for responsive layout.
 import './Styles.css';
 
 const About = () => (
   <Container className='about-container'>
     <h2 className="h2">About Me</h2>
-    <p className="p">My name is Jhonna Acebo, a 20-year-old third-year Computer Science student 
+    <p className="p">
+      My name is Jhonna Acebo, a 20-year-old third-year Computer Science student 
       at the University of Cabuyao. I am currently focused on learning Web Development, 
       exploring both front-end and back-end technologies to build dynamic and user-friendly websites. 
       Although I'm still honing my programming skills, I am passionate about creating well-designed 
       and functional web applications. I'm eager to gain more experience, work on real-world projects, 
       and continue learning and stay updated with the latest trends 
-      and best practices in the field of web development.</p>
+      and best practices in the field of web development.
+    </p>
   </Container>
 );
 
 export default About;
+
