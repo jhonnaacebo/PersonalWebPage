0cb43b2f2eabb3806ac05a855dfe988358ff1cf8
jhonnaacebo <acebojhonna72@gmail.com>
2024-10-17 08:57:41 +0800

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,8 +5,14 @@ import './Styles.css';
 const Home = () => (
   <Container className="home-container">
     <h2 className="h2">Welcome to my profile!</h2>
-    <p className="p">Hello, I am Jhonna Acebo a third year Computer Science student 
-      and my career goal is to be a proficient web developer.</p>
+    <p className="p">Hello, I am Jhonna Acebo, a third-year Computer Science student 
+      with a strong interest in becoming a skilled web developer. 
+      While programming has been challenging for me, I am committed to improving and growing in this field. 
+      My focus is on learning the fundamentals of web development and building practical skills, 
+      especially in creating responsive and user-friendly websites. 
+      I am motivated to keep exploring web technologies, sharpening my coding abilities, 
+      and gaining hands-on experience through projects that will help me grow into a confident 
+      and proficient web developer over time.</p>
   </Container>
   
 );
