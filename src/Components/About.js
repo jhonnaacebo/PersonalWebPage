e2561f01500af9f8cb412555927d47a511501120
jhonnaacebo <acebojhonna72@gmail.com>
2024-10-17 09:03:22 +0800

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,9 +5,13 @@ import './Styles.css';
 const About = () => (
   <Container className='about-container'>
     <h2 className="h2">About Me</h2>
-    <p className="p">My name is Jhonna Acebo, a 20 year old third year Computer Science Stduent at University of Cabuyao. 
-      I am currently learning Web Development.
-    </p>
+    <p className="p">My name is Jhonna Acebo, a 20-year-old third-year Computer Science student 
+      at the University of Cabuyao. I am currently focused on learning Web Development, 
+      exploring both front-end and back-end technologies to build dynamic and user-friendly websites. 
+      Although I'm still honing my programming skills, I am passionate about creating well-designed 
+      and functional web applications. I'm eager to gain more experience, work on real-world projects, 
+      and continue learning and stay updated with the latest trends 
+      and best practices in the field of web development.</p>
   </Container>
 );
 
